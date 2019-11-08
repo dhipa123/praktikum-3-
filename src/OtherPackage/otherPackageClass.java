d8f913ad1aa06e7a703f6f1958bd6c3d6dff293a
dhipa123 <dipaandaresta@gmail.com>
2019-11-08 21:57:22 +0800

    first commit

---
+++ src/OtherPackage/otherPackageClass.java
@@ -0,0 +1,11 @@
+package OtherPackage;
+
+/**
+ * otherPackageClass
+ */
+public class otherPackageClass {
+
+    public String testOtherMethod() {
+        return "ini adalah method dari otherPackageClass pada package other package";
+    }
+}
---
+++ src/app/childModifier.java
@@ -0,0 +1,18 @@
+package app;
+ 
+/**
+ * childModifier
+ */
+class childModifier extends Modifier{
+    static int count = 0;
+ 
+    void testChild() {
+        super.defaultMethod();
+        System.out.println("ini adalah method dari child modifier");
+    }
+
+    @Override
+    void defaultMethod() {
+        System.out.println(" ini adalah override dari default method");
+    }
+}
